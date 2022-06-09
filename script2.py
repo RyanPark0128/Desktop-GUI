8ec6ed396fe54bc824aa37e8249eb1663c07a71d
RyanPark0128 <vazywon@hotmail.com>
2022-06-09 14:33:00 -0700

    sql queries on read, add, update, delete

--- script2.py
+++ script2.py
@@ -30,4 +30,24 @@ def view():
     return rows
 
 
+def delete(item):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM store WHERE item=?", (item,))
+    conn.commit()
+    conn.close()
+
+
+def update(quantity, price, item):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("UPDATE store SET quantity=?, price=? WHERE item=?",
+                (quantity, price, item,))
+    conn.commit()
+    conn.close()
+
+
+# delete('Water Glass')
+
+update(2, 5, 'Coffee Cup')
 print(view())
