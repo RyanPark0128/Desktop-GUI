87e5ee9cfc59c707dead409bff2d700fb22be265
RyanPark0128 <vazywon@hotmail.com>
2022-06-15 19:30:03 -0700

    bind functions to all the buttons

--- script3.py
+++ script3.py
@@ -6,7 +6,14 @@ def get_selected_row(event):
     global selected_tuple
     index = list1.curselection()[0]
     selected_tuple = list1.get(index)
-    print(selected_tuple)
+    e1.delete(0, END)
+    e1.insert(END, selected_tuple[1])
+    e2.delete(0, END)
+    e2.insert(END, selected_tuple[2])
+    e3.delete(0, END)
+    e3.insert(END, selected_tuple[3])
+    e4.delete(0, END)
+    e4.insert(END, selected_tuple[4])
 
 
 def view_command():
@@ -31,9 +38,18 @@ def add_command():
 
 def delete_command():
     script4.delete(selected_tuple[0])
+    view_command()
+
+
+def update_command():
+    script4.update(selected_tuple[0], title_text.get(), author_text.get(),
+                   year_text.get(), isbn_text.get())
+    view_command()
 
 
 window = Tk()
+
+window.wm_title("BookStore")
 
 l1 = Label(window, text="Title")
 l1.grid(row=0, column=0)
@@ -56,12 +72,12 @@ e2 = Entry(window, textvariable=author_text)
 e2.grid(row=0, column=3)
 
 year_text = StringVar()
-e2 = Entry(window, textvariable=year_text)
-e2.grid(row=1, column=1)
+e3 = Entry(window, textvariable=year_text)
+e3.grid(row=1, column=1)
 
 isbn_text = StringVar()
-e2 = Entry(window, textvariable=isbn_text)
-e2.grid(row=1, column=3)
+e4 = Entry(window, textvariable=isbn_text)
+e4.grid(row=1, column=3)
 
 list1 = Listbox(window, height=6, width=35)
 list1.grid(row=2, column=0, rowspan=6, columnspan=2)
@@ -83,13 +99,13 @@ b2.grid(row=3, column=3)
 b3 = Button(window, text="Add Entry", width=12, command=add_command)
 b3.grid(row=4, column=3)
 
-b4 = Button(window, text="Update", width=12)
+b4 = Button(window, text="Update", width=12, command=update_command)
 b4.grid(row=5, column=3)
 
 b5 = Button(window, text="Delete", width=12, command=delete_command)
 b5.grid(row=6, column=3)
 
-b6 = Button(window, text="Close", width=12)
+b6 = Button(window, text="Close", width=12, command=window.destroy)
 b6.grid(row=7, column=3)
 
 window.mainloop()
